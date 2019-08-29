56eb5007a2981cceb5483bc8198329b15cdc0e0e
lihongmei <1224301484@qq.com>
2019-08-29 17:48:25 +0800

    新建field-service-module

---
+++ provider-service-modules/field-service-module/src/main/java/cn/nmmpa/field/FieldSeviceApplication.java
@@ -0,0 +1,15 @@
+package cn.nmmpa.field;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lhm
+ * @date 2019/8/29 17:43
+ **/
+@SpringBootApplication
+public class FieldSeviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FieldSeviceApplication.class,args);
+    }
+}
